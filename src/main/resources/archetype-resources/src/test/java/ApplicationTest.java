afd89a918c1531f786bb4b554a36323c5f497e11
Dominik Cebula <dominikcebula@gmail.com>
2024-09-22 16:18:31 +0200

    Java 21 Spring Boot 3 Web Archetype

--- src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -3,20 +3,35 @@
 #set($symbol_escape='\' )
 package ${package};
 
-import org.junit.jupiter.api.Test;
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlPage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class ApplicationTest {
 
-    @Value(value = "${symbol_dollar}{local.server.port}")
+    @Value(value = "${local.server.port}")
     private int port;
 
-    @Test
-    void contextLoads() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "/", "/home"})
+    void shouldDisplayDefaultHelloPage(String path) throws IOException {
+        try (WebClient webClient = new WebClient()) {
+            HtmlPage page = webClient.getPage(getLocalUrl(path));
+
+            assertEquals("Welcome to Spring Boot 3 Web Application.", page.getBody().asNormalizedText());
+        }
+    }
+
+    private String getLocalUrl(String path) {
+        return "http://localhost:%d%s".formatted(port, path);
     }
 }
